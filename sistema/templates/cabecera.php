6099285a174832207aa053b58162821dbaed19d2
diegoteos <diegoteos@hotmail.com>
2022-09-27 23:24:55 -0600

    title

--- sistema/templates/cabecera.php
+++ sistema/templates/cabecera.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<title>Sistema de alumnos</title>
 </head>
 
 <body>
