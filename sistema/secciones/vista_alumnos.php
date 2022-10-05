fd03a553477df8bd29e88f7af3beeca11f2f2802
diegoteos <diegoteos@hotmail.com>
2022-10-04 22:19:12 -0600

    alumnos vista

--- sistema/secciones/vista_alumnos.php
+++ sistema/secciones/vista_alumnos.php
@@ -3,10 +3,50 @@ include('../templates/cabecera.php');
 ?>
 
 
+<div class="row">
+    <div class="col-5">
+        <form action="" method="post">
+            <div class="card">
+                <div class="card-header">
+                    Alumnos
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Text</p>
+                </div>
+                <div class="card-footer text-muted">
+                    Footer
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="col-7">
+        <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row"></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td scope="row"></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
 
-Control de alumnos
-vista alumnos
-
+</div>
 
 <?php
 include('../templates/pie.php');
